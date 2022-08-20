23c86ad18a30c243a6f3169d450429e76a1476f4
sdphat <77732077+sdphat@users.noreply.github.com>
2022-08-20 14:51:15 +0700

    add global styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <h2>
-      App Placeholder
-    </h2>
+    <>
+      <h2>
+        App Placeholder
+      </h2>
+      <a href="/a">link</a>
+      <button type="button">button</button>
+      <span>span</span>
+      <div>div</div>
+    </>
   );
 }
 
