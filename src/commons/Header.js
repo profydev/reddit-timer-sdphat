02bb528939e8f64c247f17aa16a19f234764f9c7
sdphat <77732077+sdphat@users.noreply.github.com>
2022-08-21 14:02:20 +0700

    Fix Search href location

--- src/commons/Header.js
+++ src/commons/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './assets/logo.svg';
+import logo from '../assets/logo.svg';
 
 const Header = () => (
   <header className="header">
@@ -8,7 +8,7 @@ const Header = () => (
       <img src={logo} alt="app logo" />
     </Link>
     <div className="header-links-wrapper">
-      <Link className="header-link" to="/search">Search</Link>
+      <Link className="header-link" to="/search/javascript">Search</Link>
       <Link className="header-link" to="/#how-it-works">How it works</Link>
       <Link className="header-link" to="/#about">About</Link>
     </div>
