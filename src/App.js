48383014ee1cee0b8a60a42d417c90828d0b9326
sdphat <77732077+sdphat@users.noreply.github.com>
2022-08-22 19:24:43 +0700

    Header (#3)
    * Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './commons/Header';
 import Landing from './routes/Landing';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/search" />
---
+++ src/commons/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+
+const Header = () => (
+  <header className="header">
+    <Link to="/" className="app-logo">
+      <img src={logo} alt="app logo" />
+    </Link>
+    <div className="header-links-wrapper">
+      <Link className="header-link" to="/search/javascript">Search</Link>
+      <Link className="header-link" to="/#how-it-works">How it works</Link>
+      <Link className="header-link" to="/#about">About</Link>
+    </div>
+  </header>
+);
+
+export default Header;
