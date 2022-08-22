04ea981981420c4bd6ba59c7a21fd12adde0b0d2
sdphat <77732077+sdphat@users.noreply.github.com>
2022-08-22 19:11:46 +0700

    Use max-width instead of padding

--- src/commons/Footer.js
+++ src/commons/Footer.js
@@ -4,11 +4,15 @@ import footerLogo from '../assets/footer_logo.svg';
 
 const Footer = () => (
   <footer className="footer">
-    <a href="https://profy.dev/employers" className="footer-link">profy.dev</a>
+    <div style={{ flex: 1, textAlign: 'left' }}>
+      <a href="https://profy.dev/employers" className="footer-link">profy.dev</a>
+    </div>
     <Link to="/" className="footer-logo">
       <img src={footerLogo} style={{ display: 'block' }} alt="Footer logo" />
     </Link>
-    <Link to="/terms" className="footer-link">Terms & Privacy</Link>
+    <div style={{ flex: 1, textAlign: 'right' }}>
+      <Link to="/terms" className="footer-link">Terms & Privacy</Link>
+    </div>
   </footer>
 );
 
